00a9b566430f105cba6f6f71154356da09e1fe32
Connor Shearer <connor_shearer@outlook.com>
2022-01-17 23:59:00 +0000

    Add in Application Insights services

--- Arasaki/Server/Program.cs
+++ Arasaki/Server/Program.cs
@@ -1,10 +1,16 @@
 using System.IO.Compression;
 
+using Microsoft.ApplicationInsights.AspNetCore.Extensions;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Identity.Web;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+#if DEBUG
+builder.Services.AddApplicationInsightsTelemetry(new ApplicationInsightsServiceOptions { ConnectionString = "00000000-0000-0000-0000-000000000000" });
+#else
+builder.Services.AddApplicationInsightsTelemetry(new ApplicationInsightsServiceOptions { ConnectionString = Services.Configuration["APPINSIGHTS_CONNECTIONSTRING"] });
+#endif
 //builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
