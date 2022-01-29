93dd8bfc370a4876f15457c69b679e0e65f27b1a
Connor Shearer <connor_shearer@outlook.com>
2022-01-29 16:06:00 +0000

    Move the out of WASM runtime script into the server HTML

--- Arasaki/Client/wwwroot-dev/ts/core.ts
+++ Arasaki/Client/wwwroot-dev/ts/core.ts
@@ -3,29 +3,13 @@ interface Window
     runtime: any;
 }
 
-var Blazor: any
 var GLOBAL: any
-
-function setupAssembly() 
+GLOBAL.JSRuntime = null
+GLOBAL.runtime = (ref: any) => { if (GLOBAL.JSRuntime === null) GLOBAL.JSRuntime = ref }
+(() =>
 {
-    let server = document.getElementById('blazorScript')
-    server?.parentNode?.removeChild(server)
-    let auth = document.createElement('script')
-    auth.type = 'text/javascript'
-    auth.src = '_content/Microsoft.Authentication.WebAssembly.Msal/AuthenticationService.js'
-    document.body.appendChild(auth)
-    let wasm = document.createElement('script')
-    wasm.type = 'text/javascript'
-    wasm.src = '_framework/blazor.webassembly.js'
-    document.body.appendChild(wasm)
-
-    GLOBAL.JSRuntime = null
-    GLOBAL.runtime = (ref: any) => { if (GLOBAL.JSRuntime === null) GLOBAL.JSRuntime = ref }
-    (() =>
+    window.runtime = 
     {
-        window.runtime = 
-        {
 
-        }
-    })()
-}
+    }
+})()
