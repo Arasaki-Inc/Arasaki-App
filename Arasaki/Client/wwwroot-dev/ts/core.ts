094ecdfab8a1e4d81b17659e3c12620c5d92e1d6
Connor Shearer <connor_shearer@outlook.com>
2022-01-29 15:48:58 +0000

    Get Blazor Server to Blazor WebAssembly working

--- Arasaki/Client/wwwroot-dev/ts/core.ts
+++ Arasaki/Client/wwwroot-dev/ts/core.ts
@@ -1,1 +1,31 @@
-console.log()
+interface Window 
+{
+    runtime: any;
+}
+
+var Blazor: any
+var GLOBAL: any
+
+function setupAssembly() 
+{
+    let server = document.getElementById('blazorScript')
+    server?.parentNode?.removeChild(server)
+    let auth = document.createElement('script')
+    auth.type = 'text/javascript'
+    auth.src = '_content/Microsoft.Authentication.WebAssembly.Msal/AuthenticationService.js'
+    document.body.appendChild(auth)
+    let wasm = document.createElement('script')
+    wasm.type = 'text/javascript'
+    wasm.src = '_framework/blazor.webassembly.js'
+    document.body.appendChild(wasm)
+
+    GLOBAL.JSRuntime = null
+    GLOBAL.runtime = (ref: any) => { if (GLOBAL.JSRuntime === null) GLOBAL.JSRuntime = ref }
+    (() =>
+    {
+        window.runtime = 
+        {
+
+        }
+    })()
+}
