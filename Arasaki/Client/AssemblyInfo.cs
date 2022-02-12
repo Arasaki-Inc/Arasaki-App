be86210580a66e159cf7b25ec33849a6c73920a1
Connor Shearer <connor_shearer@outlook.com>
2022-02-12 12:05:58 +0000

    Clean up all project files and declare assembly info manually as well as declare build datetime

---
+++ Arasaki/Client/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCopyright("Connor 'Stryxus' Shearer")]
+[assembly: AssemblyProduct("Arasaki App")]
+[assembly: AssemblyTrademark("Arasaki")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: Guid("16ba1a5e-446b-4d8b-8f05-04db7a110fa7")]
+[assembly: ComVisible(false)]
