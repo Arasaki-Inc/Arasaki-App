e0277a8adf66518510db2af2fef59326af1f04ee
Connor Shearer <connor_shearer@outlook.com>
2022-02-12 12:06:19 +0000

    Move References to Runtime and add Version getters

--- Arasaki/Shared/References.cs
+++
@@ -1,9 +0,0 @@
-﻿public static class References
-{
-    public static string YearVersion { get; set; } = "2022";
-    public static string MonthVersion { get; set; } = "01";
-    public static string DayVersion { get; set; } = "01";
-    public static string IterationVersion { get; set; } = "a";
-    public static string VersionString { get { return YearVersion + "." + MonthVersion + "." + DayVersion + IterationVersion; } }
-    public static bool IsDevelopmentMode { get; set; } = false;
-}
--- Arasaki/Shared/Runtime.cs
+++ Arasaki/Shared/Runtime.cs
@@ -1,8 +1,36 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Globalization;
+using System.Reflection;
+using System.Runtime.InteropServices;
 
 public static class Runtime
 {
-    public static bool IsMono => RuntimeInformation.FrameworkDescription.Contains("Mono");
+    public static Assembly RuntimeAssembly { get => Assembly.GetCallingAssembly(); }
+    public static Assembly LibraryAssembly { get => Assembly.GetExecutingAssembly(); }
 
+    public static bool IsMono => RuntimeInformation.FrameworkDescription.Contains("Mono");
     public static bool IsWASM => RuntimeInformation.OSDescription.Contains("Browser");
+#if DEBUG
+    public static bool IsDevelopmentMode { get; } = true;
+#else
+    public static bool IsDevelopmentMode { get; } = false;
+#endif
+
+    public static DateTime RuntimeBuildDate
+    {
+        get
+        {
+            const string BuildVersionMetadataPrefix = "+build";
+            AssemblyInformationalVersionAttribute attribute = RuntimeAssembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+            if (attribute?.InformationalVersion != null)
+            {
+                string value = attribute.InformationalVersion;
+                int index = value.IndexOf(BuildVersionMetadataPrefix);
+                if (index > 0 && DateTime.TryParseExact(value[(index + BuildVersionMetadataPrefix.Length)..], "yyyyMMddHHmmss", CultureInfo.InvariantCulture, DateTimeStyles.None, out var result)) return result;
+            }
+            return default;
+        }
+    }
+
+    public static long Version { get => long.Parse(RuntimeBuildDate.Year + string.Empty + RuntimeBuildDate.Month + string.Empty + RuntimeBuildDate.Day + string.Empty + RuntimeBuildDate.Hour + string.Empty +
+        RuntimeBuildDate.Minute + string.Empty + RuntimeBuildDate.Second); }
 }
