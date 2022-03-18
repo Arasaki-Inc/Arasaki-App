d29855fc3eafb06912b41389f425040b52ad26bf
Connor Shearer <connor_shearer@outlook.com>
2022-03-18 17:52:01 +0000

    Add firewall rules to the setup.cmd for external testing

--- setup.cmd
+++ setup.cmd
@@ -3,4 +3,6 @@ cd "Bundler"
 start cmd /c npm i
 cd "..\Arasaki\Client\wwwroot-dev"
 start cmd /c npm i
+start cmd netsh http add urlacl url=http://*:7107/ user=Interactive listen=yes
+start cmd netsh advfirewall firewall add rule name="Https Port 7107" dir=in action=allow protocol=TCP localport=7107
 exit
