210d5029f250703f9cc14a009cb6ca93a6e9b57a
Connor Shearer <connor_shearer@outlook.com>
2022-03-31 02:10:12 +0100

    Just preset the page context list

--- UEFI/Data/States/UIState.cs
+++ UEFI/Data/States/UIState.cs
@@ -3,7 +3,44 @@
 public class UIState
 {
     public event Action PageContextHasChanged;
-    public List<PageContext> PageContexts = new();
+    public List<PageContext> PageContexts = new()
+            {
+                new PageContext
+                {
+                    PublicName = "Arasaki UEFI",
+                    PrivateName = "uefi",
+                    RelativeURLs = new() { "/uefi", "/" },
+                    BackgroundRelativeURL = "/img/server/backgrounds/uefi.svg"
+                },
+                new PageContext
+                {
+                    PublicName = "App Settings",
+                    PrivateName = "settings",
+                    RelativeURLs = new() { "/uefi/settings" },
+                    BackgroundRelativeURL = "/img/server/backgrounds/settings.svg"
+                },
+                new PageContext
+                {
+                    PublicName = "Permissions",
+                    PrivateName = "permissions",
+                    RelativeURLs = new() { "/uefi/perms" },
+                    BackgroundRelativeURL = "/img/server/backgrounds/permissions.svg"
+                },
+                new PageContext
+                {
+                    PublicName = "Changelog",
+                    PrivateName = "changelog",
+                    RelativeURLs = new() { "/uefi/changelog" },
+                    BackgroundRelativeURL = "/img/server/backgrounds/changelog.svg"
+                },
+                new PageContext
+                {
+                    PublicName = "About",
+                    PrivateName = "about",
+                    RelativeURLs = new() { "/uefi/about" },
+                    BackgroundRelativeURL = "/img/server/backgrounds/about.svg"
+                }
+            };
     private PageContext pageContext;
     public PageContext CurrentPageContext
     {
